b2ffbc5664f8845520629c1816e84a43196893ae
nechukhaeva <yana.nechukhaeva.00@mail.ru>
2021-11-11 23:00:35 +1000

    changes in laba5

--- db.js
+++ db.js
@@ -1,50 +1,48 @@
-'use strict'
-
-const Database = require('better-sqlite3')
+const Database = require('better-sqlite3');
 
 class Db {
     constructor(file) {
-        this.db = new Database(file)
+        this.db = new Database(file);
 
-        this.createTables()
+        this.createTables();
     }
 
     createTables(){
-        let sql = ''
+        let sql = '';
 
         sql = `CREATE TABLE IF NOT EXISTS files (
             id integer PRIMARY KEY,
             name text,
             size text,
             path text
-		)`
-        this.db.prepare(sql).run()
+		)`;
+        this.db.prepare(sql).run();
     }
 
     insertFiles(files){
         if(files){
             for(let file of files){
-                this.db.prepare('INSERT INTO files (name, size, path) VALUES (?,?,?)').run(file.name, file.size, file.path)
+                this.db.prepare('INSERT INTO files (name, size, path) VALUES (?,?,?)').run(file.name, file.size, file.path);
             }
-            return this.selectFiles()
+            return this.selectFiles();
         }
-        return null
+        return null;
     }
 
     selectFiles(){
-        return this.db.prepare(`SELECT * FROM files`).all()
+        return this.db.prepare(`SELECT * FROM files`).all();
     }
 
     selectPathFile(name){
-        return this.db.prepare(`SELECT path FROM files WHERE name = ?`).get(name).path
+        return this.db.prepare(`SELECT path FROM files WHERE name = ?`).get(name).path;
     }
 
     deleteFile(name){
-        let file = this.db.prepare(`SELECT * FROM files WHERE name = ?`).get(name)
-        let stmt = this.db.prepare('DELETE FROM files WHERE name = ?').run(name)
-        if (stmt.changes) return file
-        return null 
+        let file = this.db.prepare(`SELECT * FROM files WHERE name = ?`).get(name);
+        let stmt = this.db.prepare('DELETE FROM files WHERE name = ?').run(name);
+        if (stmt.changes) return file;
+        return null ;
     }
 }
 
-module.exports = Db
+module.exports = Db;
--- server.js
+++ server.js
@@ -1,97 +1,93 @@
-'use strict'
+const express = require("express");
+const DB = require("./db");
 
-const express = require('express')
-const DB = require('./db')
+const db = new DB("files.sqlite");
+const app = express();
+const router = express.Router();
+const fs = require("fs");
+const multer = require("multer");
 
-const db = new DB('files.sqlite')
-const app = express()
-const router = express.Router()
-const fs = require('fs')
-const multer = require('multer')
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
-router.use(express.json())
-router.use(express.urlencoded({ extended: true }))
-
-let dir = './upload'
+let dir = "./upload";
 if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir)
-    console.log('create dir', dir)
+    fs.mkdirSync(dir);
+    console.log("create dir", dir);
 }
 
-
-var storage = multer.diskStorage({
+let storage = multer.diskStorage({
     destination: function (req, file, cb){
-        cb(null, './upload')
+        cb(null, "./upload");
     }, 
     filename: function(req, file, cb){
-        cb(null, file.fieldname)
+        cb(null, file.fieldname);
     }
-})
-var upload = new multer({storage: storage})
+});
+let upload = new multer({storage: storage});
 
-router.post('/files', upload.any(), (req, res) => {
-    console.log("save files")
-    var data = []
+router.post("/files", upload.any(), (req, res) => {
+    console.log("save files");
+    let data = [];
     for (let file of req.files) {
         let object = {
             name: file.filename,
-            path: file.destination + '/',
+            path: file.destination + "/",
             size: file.size
-        }
-        console.log(file)
-        data.push(object)
+        };
+        console.log(file);
+        data.push(object);
     }
-    const files = db.insertFiles(data)
+    const files = db.insertFiles(data);
     if (files) {
-        console.log('files', files)
-        res.status(200).send({ success: true, status: 'ok', files })
+        console.log("files", files);
+        res.status(200).send({ success: true, status: "ok", files });
     } else {
-        res.status(200).send({ success: false, status: 'no files save' })
-    } 
-})
+        res.status(200).send({ success: false, status: "no files save" });
+    }
+});
 
-router.get('/files', (req, res) => {
-    console.log('get files')
-    const files = db.selectFiles()
+router.get("/files", (req, res) => {
+    console.log("get files");
+    const files = db.selectFiles();
     if (files.length != 0){
-        res.status(200).send({success: true, msg: 'ok', files})
+        res.status(200).send({success: true, msg: "ok", files});
     } else {
-        res.status(200).send({success: false, msg: 'no files found'})
+        res.status(200).send({success: false, msg: "no files found"});
     }
-})
+});
 
-router.get('/file', (req, res) => {
-    console.log('get file', req.query.name)
-    const path = db.selectPathFile(req.query.name)
-    if (path || path != ''){
-        res.status(200).sendFile(__dirname + path.replace('./', '/') + req.query.name)
+router.get("/file", (req, res) => {
+    console.log("get file", req.query.name);
+    const path = db.selectPathFile(req.query.name);
+    if (path || path != ""){
+        res.status(200).sendFile(__dirname + path.replace("./", "/") + req.query.name);
     } else {
-        res.status(200).send({success: false, msg: 'no file found'})
+        res.status(200).send({success: false, msg: "no file found"});
     }
-})
+});
 
-router.delete('/file', (req, res) => {
-    console.log('delete file', req.query.name)
-    const file = db.deleteFile(req.query.name)
+router.delete("/file", (req, res) => {
+    console.log("delete file", req.query.name);
+    const file = db.deleteFile(req.query.name);
     if (file){
-        fs.unlink(__dirname + file.path.replace('./', '/') + file.name, function(err){
+        fs.unlink(__dirname + file.path.replace("./", "/") + file.name, function(err){
             if(err) {
-                console.log(err)
-                res.status(200).send({success: false, msg: 'no file deleted'})
+                console.log(err);
+                res.status(200).send({success: false, msg: "no file deleted"});
             } else{
-                res.status(200).send({success: true, msg: 'ok', file})
+                res.status(200).send({success: true, msg: "ok", file});
             }
-       })
+       });
     } else {
-        res.status(200).send({success: false, msg: 'no file found'})
+        res.status(200).send({success: false, msg: "no file found"});
     }
-})
+});
 
-app.use(router)
+app.use(router);
 
-let port = 3000
-let hostname = "0.0.0.0"
+let port = 3000;
 
-app.listen(port, hostname, function () {
-    console.log('start api', 'listening port: ' + port)
-})
+app.listen(port, function () {
+    console.log("start api", "listening port: " + port);
+});
